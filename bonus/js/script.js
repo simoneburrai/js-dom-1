375b69ad0748e96bf5266d4666aa1053485ad03f
simoneburrai <burrai.s98@gmail.com>
2025-03-04 19:52:50 +0100

    feat: bonus completed

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,11 +1,21 @@
 
 
 const lampElement = document.getElementById("lamp");
-lampElement.addEventListener("click", function(){
-    const lampOn = "../img/yellow_lamp.png";
-        lampElement.src = lampOn;
-        console.log(lampElement.src);
-        lampElement.classList.add("position");
+const lampOn = "../img/yellow_lamp.png";
+const lampOff = "../img/white_lamp.png";
+const lampButton = document.getElementById("turnlamp");
+lampButton.innerText = "Accendi";
+
+lampButton.addEventListener("click", function(){
+    if (lampButton.innerText.includes("Accendi")){
+                lampElement.src = lampOn;
+                lampElement.classList.add("position");
+                lampButton.innerHTML = "Spegni";
+    } else {
+        lampElement.src = lampOff;
+        lampElement.classList.remove("position");
+        lampButton.innerHTML = "Accendi";
+    }
 })
 
 
